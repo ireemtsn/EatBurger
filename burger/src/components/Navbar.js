a8107d73394ffd190f64f194283c46f110e08c79
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-04-17 15:03:25 +0300

    liste yapısının link ile oluşturulması

--- burger/src/components/Navbar.js
+++ burger/src/components/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import BurgerLogo from '../assets/logo2.jpg';
+import {Link} from 'react-router-dom'
+
 
 export const Navbar = () => {
   return(
     <div className='navbar'>
         <div className='main'>
+        <div>
             <img src={BurgerLogo} alt=""></img>
+            <Link to="/">Anasayfa
+            </Link>
+            <Link to="/menu">Menü
+            </Link>
+            <Link to="/about">Hakkımızda
+            </Link>
+            <Link to="/contact">İletişim
+            </Link>
+            </div>
+       
         </div>
-
+      
+        
     </div>
   ) 
 };
