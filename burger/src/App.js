0006c274e9c75fa6052b7e60d9a996058e1b1862
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-04-17 14:05:35 +0300

    route elementlerinin oluşturulması

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      irem
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/menu" exact element={<Menu />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
+      </Routes>
     </div>
   );
 }
