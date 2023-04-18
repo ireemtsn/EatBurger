13d58e34e4ff15b52946bd34880cadcc5612b347
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-04-18 14:24:01 +0300

    css düzenlemeleri

--- burger/src/components/Footer.js
+++ burger/src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import '../styles/Footer.css';
+
+
+
 
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="socialMedia">
-    
+      <FaFacebook />
+      <FaInstagram />
+      <FaTwitter />
+      <FaLinkedin />
       </div>
       <p>Tüm hakları saklıdır @2023 | Eat Burger</p>
     </div>
