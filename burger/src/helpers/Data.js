c435b2003f6c7108e443fb294be8a3faac5cb5df
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-04-18 15:52:50 +0300

    data js oluşturulması

---
+++ burger/src/helpers/Data.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BurgerImage from '../assets/burger.jpg';
+
+export const Data = [
+  {
+    name: 'Efso Burger',
+    image: BurgerImage,
+    content: '150 gr kasap Burger,Patates,Turşu',
+    price: 250,
+  } ];
