2ed70738962781ea5c9e84e2b49f9534ab28c90b
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-04-18 12:43:09 +0300

    bg image verilmesi

--- burger/src/components/Home.js
+++ burger/src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import bannerImage from '../assets/banneryeni.jpg'
 
 export const Home = () => {
   return(
-    <div>
-      <div>
+    <div className='mainPage' style={{backgroundImage: 'url(${bannerImage})'}}>
+      <div className='order'>
         <Link to="/menu">
           <button>Sipariş Ver</button>
         </Link>
