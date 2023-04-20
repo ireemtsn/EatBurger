7772b3fd3d579a6afffffafee6d6380131f1afbb
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-04-20 11:33:19 +0300

    ikon paketinin tekrardan indirilmesi

--- burger/src/components/Footer.js
+++ burger/src/components/Footer.js
@@ -7,14 +7,14 @@ import '../styles/Footer.css';
 
 export const Footer = () => {
   return (
-    <div className="footer">
-      <div className="socialMedia">
+     <div className="footer">
+       <div className="socialMedia">
       <FaFacebook />
-      <FaInstagram />
-      <FaTwitter />
-      <FaLinkedin />
-      </div>
-      <p>Tüm hakları saklıdır @2023 | Eat Burger</p>
-    </div>
-  );
+       <FaInstagram />
+       <FaTwitter />
+       <FaLinkedin />
+       </div>
+       <p>Tüm hakları saklıdır @2023 | Eat Burger</p>
+     </div>
+   );
 };
